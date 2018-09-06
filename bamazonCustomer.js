80e91b7ad304ab806b9de0c8603c26ce1d97cc48
erandro <erandro@gmail.com>
2018-09-06 15:55:23 -0500

    fixed bug in bamazon.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -43,30 +43,22 @@ function askItemIdAndAmount() {
             }
         ]).then(function (userInput) {
 
-            var product_stock = checkProductStock(userInput.id);
-            //because checkProductStock() is stuck:
-            //console.log(product_stock); --> undifined
-            //just to be able to continued I gave product_stock a value:
-            product_stock = 12;
-
-            var new_amonut = product_stock - userInput.amount
-            if (new_amonut < 0) {
-                console.log("There are only " + product_stock + " in stock. Please select product id and amount again");
-                askItemIdAndAmount();
-            } else {
-                editProductAmountInDatabase(new_amonut, userInput.id);
-                var purchase_price = showPurchaseCost(userInput.amount, userInput.id);
-            };
+            connection.query("SELECT stock_quantity FROM products where item_id = " + userInput.id, function (err, res) {
+                if (err) throw err;
+                product_stock = res[0].stock_quantity;
+                var new_amonut = product_stock - userInput.amount
+                if (new_amonut < 0) {
+                    console.log("There are only " + product_stock + " in stock. Please select product id and amount again");
+                    askItemIdAndAmount();
+                } else {
+                    editProductAmountInDatabase(new_amonut, userInput.id);
+                    var purchase_price = showPurchaseCost(userInput.amount, userInput.id);
+                };
+            });
         });
     });
 };
-function checkProductStock(id) {
-    connection.query("SELECT stock_quantity FROM products where item_id = " + id, function (err, res) {
-        if (err) throw err;
-        return res[0].stock_quantity;
-        // this function is stuck (when I do "return") - not returning the amount
-    });
-};
+
 function editProductAmountInDatabase(amount, id) {
     connection.query("UPDATE products SET ? WHERE ?",
         [
@@ -81,6 +73,7 @@ function editProductAmountInDatabase(amount, id) {
             console.log("*products amount have been updated!*\n");
         });
 };
+
 function showPurchaseCost(amount, id) {
     connection.query("SELECT price FROM products where item_id = " + id, function (err, res) {
         if (err) throw err;
